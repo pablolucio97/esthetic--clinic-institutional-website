c676b9e4f5bb8dcb5e25635ba0700442b30cd08f
Pablo Silva <pablolucio_@hotmail.com>
2022-06-27 09:05:27 -0300

    Initial commit

---
+++ src/styles/index.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+export const BannerSection = styled.section`
+  display: flex;
+`;
+
+export const IntroductionSection = styled.section`
+  display: flex;
+`;
+
+export const MetricsSection = styled.section`
+  display: flex;
+`;
+
+export const TestimonialsSection = styled.section`
+  display: flex;
+`;
+
+export const FaqSection = styled.section`
+  display: flex;
+`;
+
+export const PurchaseSection = styled.section`
+  display: flex;
+`;
+
+export const BannerSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const IntroductionSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const MetricsSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const TestimonialSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const FaqSectionSectionContainer = styled.div`
+  display: flex;
+`;
+
+export const PurchaseSectionSectionContainer = styled.div`
+  display: flex;
+`;
